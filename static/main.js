e24120e7cfc9c6503c7b2f61b8db627afe324a08
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2023-08-11 13:12:34 -0400

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,18 @@
+const URL_PARAMS = new URLSearchParams(window.location.search);
+const TOKEN = URL_PARAMS.get('token');
+
+// Show an element
+const show = (selector) => {
+  document.querySelector(selector).style.display = 'block';
+};
+
+// Hide an element
+const hide = (selector) => {
+  document.querySelector(selector).style.display = 'none';
+  //document.querySelector(selector).innerHTML = '';
+};
+
+if (TOKEN) {
+  hide('.content.unauthorized');
+  show('.content.authorized');
+}
